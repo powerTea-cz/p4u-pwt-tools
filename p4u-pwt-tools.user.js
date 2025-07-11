8115e4eaeac0bc8ff583ba9f123d75820f97c379
powerTea-cz <powertea.cz@gmail.com>
2025-07-11 10:31:27 +0200

    fix: Remove SA from p4u tools

--- p4u-pwt-tools.user.js
+++ p4u-pwt-tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         p4u-pwt-tools
 // @description  Tools for +4U made by powerTea
-// @version      1.1.1
+// @version      1.2
 // @namespace    https://uuos9.plus4u.net/
 // @homepage     https://github.com/powerTea-cz/p4u-pwt-tools
 // @author       powerTea-cz
@@ -44,34 +44,8 @@ margin-bottom: 8px;
 
 `);
 
-const SWF_UNIT_NAME = "D9";
-
 (function () {
   "use strict";
-  function getWeekNumber(d) {
-    // Copy date so don't modify original
-    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-    // Get first day of year
-    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    // Calculate full weeks to nearest Thursday
-    const weekNo = Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
-    // Return array of year and week number
-    return [d.getUTCFullYear(), weekNo];
-  }
-
-  function getCurrentSA() {
-    document.getElementById("system-menu-icon-link").click();
-    let currentWeek = getWeekNumber(new Date());
-    console.log("SA for week", currentWeek);
-    setTimeout(function () {
-      document.getElementById("quickFindInput").value =
-        `UNI-BT:SWF.${SWF_UNIT_NAME}/SA${currentWeek[0]}${currentWeek[1]}`;
-      document.getElementById("quickfindLink").click();
-    }, 200);
-  }
 
   function getTsr() {
     document.getElementById("system-menu-icon-link").click();
@@ -88,9 +62,6 @@ const SWF_UNIT_NAME = "D9";
     .appendTo("body");
 
   $("<span>").text("Tools").appendTo(panel);
-
-  $("<button>").html("Aktuální SA").click(getCurrentSA).appendTo(panel);
-
   $("<button>").html("TSR").click(getTsr).appendTo(panel);
 
   $("body").append(panel);
